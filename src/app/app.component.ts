ce137b80c47ce0a05cc6acba21260c11462c671f
Shaikh Irshad <shaikhij2000@gmail.com>
2021-05-19 12:06:02 +0530

    ui fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {  Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import CustomStore from 'devextreme/data/custom_store';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'devexpress-demo';
+  dataSource: any = {};
+
+  constructor(httpClient: HttpClient) {
+      function isNotEmpty(value: any): boolean {
+          return value !== undefined && value !== null && value !== "";
+      }
+      this.dataSource = new CustomStore({
+          key: "counter",
+          load: function (loadOptions: any) {
+              let params: HttpParams = new HttpParams();
+              [
+                  "skip",
+                  "take",
+                  "requireTotalCount",
+                  "requireGroupCount",
+                  "sort",
+                  "filter",
+                  "totalSummary",
+                  "group",
+                  "groupSummary"
+              ].forEach(function(i) {
+                  if (i in loadOptions && isNotEmpty(loadOptions[i]))
+                      params = params.set(i, JSON.stringify(loadOptions[i]));
+              });
+              const apiUrl = 'https://localhost:44318/api/User/GetData';
+              //const apiUrl = 'https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/orders';
+              return httpClient.get(apiUrl, { params: params })
+                  .toPromise()
+                  .then((data: any) => {
+                      return {
+                          data: data.data,
+                          totalCount: data.totalCount,
+                          summary: data.summary,
+                          groupCount: data.groupCount
+                      };
+                  })
+                  .catch(error => { throw 'Data Loading Error' });
+          }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DxDataGridModule } from 'devextreme-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxDataGridModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
